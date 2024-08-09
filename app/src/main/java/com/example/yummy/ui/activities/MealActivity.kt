9c8e3e1dd3b1a71c4e16b4e87c8018d61e0f0865
Allan Mathenge <allanmathenge67@gmail.com>
2024-08-09 16:49:07 +0300

    updated the progressbar and set the click listener for the youtube image

--- app/src/main/java/com/example/yummy/ui/activities/MealActivity.kt
+++ app/src/main/java/com/example/yummy/ui/activities/MealActivity.kt
@@ -2,7 +2,9 @@ package com.example.yummy.ui.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -18,6 +20,7 @@ class MealActivity : AppCompatActivity() {
     private lateinit var mealName: String
     private lateinit var binding: ActivityMealBinding
     private lateinit var mealMVVM: MealViewModel
+    private lateinit var youtubeLink: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
@@ -29,8 +32,12 @@ class MealActivity : AppCompatActivity() {
         //Function call to get the random meal information from the intent
         getMealInformationFromIntent()
 
+
         //Function call to set the information for the random meal ... image and the title
         setInformationInViews()
+
+        //Function call to invoke loading case ... the call is just before requesting for details from the api through the method that is in the viewmodel
+        loadingCase()
 
         //calling the getMealDetail function using the meal view model instance that we just created
         mealMVVM.getMealDetail(mealId)
@@ -38,16 +45,29 @@ class MealActivity : AppCompatActivity() {
         //A function call to observe meal detail live data
         observeMealDetailLiveData()
 
+        //function call for clicking the youtube image
+        onYouTubeImageClick()
 
+
+    }
+
+    private fun onYouTubeImageClick() {
+        binding.youtube.setOnClickListener(){
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+        }
     }
 
     @SuppressLint("SetTextI18n")
     private fun observeMealDetailLiveData() {
         mealMVVM.observeMealDetailsLiveData().observe(this){mealDetails ->
             mealDetails?.apply {
+
+                //function call to invoke response case ... we call this function here since on observing the live data, when the live data changes, we are getting a response
+                onResponseCase()
                 binding.tvCategoryInfo.text = "Category: ${mealDetails.strCategory}"
                 binding.tvAreaInfo.text = "Location: ${mealDetails.strArea}"
                 binding.tvContent.text = mealDetails.strInstructions
+                youtubeLink = mealDetails.strYoutube
 
             }
 
@@ -70,5 +90,24 @@ class MealActivity : AppCompatActivity() {
         mealId = intent.getStringExtra(HomeFragment.MEAL_ID).toString()
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB).toString()
 
+    }
+
+
+    private fun loadingCase(){
+        binding.progressBar.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.btnAddToFavorite.visibility = View.INVISIBLE
+        binding.youtube.visibility = View.INVISIBLE
+        binding.tvAreaInfo.visibility = View.INVISIBLE
+        binding.tvCategoryInfo.visibility = View.INVISIBLE
+    }
+
+    private fun onResponseCase(){
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.btnAddToFavorite.visibility = View.VISIBLE
+        binding.youtube.visibility = View.VISIBLE
+        binding.tvAreaInfo.visibility = View.VISIBLE
+        binding.tvCategoryInfo.visibility = View.VISIBLE
     }
 }
